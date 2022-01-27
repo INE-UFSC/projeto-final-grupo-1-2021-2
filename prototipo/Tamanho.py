a1e8fa7f0452d6b18bbc3f3034f1bab95dbf3910
TorresLino <arthur.torres.de.lino@gmail.com>
2022-01-27 19:55:23 -0300

    classe Tamanho

---
+++ prototipo/Tamanho.py
@@ -0,0 +1,20 @@
+class Tamanho:
+    def __init__(self, altura: float, largura: float):
+        self.__altura = altura
+        self.__largura = largura
+
+    @property
+    def altura(self) -> float:
+        return self.__altura
+
+    @property
+    def largura(self) -> float:
+        return self.__largura
+
+    @altura.setter
+    def altura(self, altura:float):
+        self.__altura = altura
+
+    @largura.setter
+    def largura(self, largura:float):
+        self.__largura = largura
