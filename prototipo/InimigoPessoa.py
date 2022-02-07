5b78359322cb6f5621fd49431137b185dab17c55
gabi-fs <gabrielafdasilveira@gmail.com>
2022-02-07 19:32:37 -0300

    adicionando método desenhar aos móveis

--- prototipo/InimigoPessoa.py
+++ prototipo/InimigoPessoa.py
@@ -1,22 +1,33 @@
+import pygame
 from Movel import Movel
 from Coordenada import Coordenada
 from Tamanho import Tamanho
 
+
 class InimigoPessoa(Movel):
-    def __init__(self, spawn:Coordenada, tamanho:Tamanho, velocidade:float, raio_deslocamento:float, raio_deteccao:float):
-        super.__init__(spawn, tamanho, velocidade)
+    def __init__(self, spawn: Coordenada, tamanho: Tamanho, velocidade: float, raio_deslocamento: float, raio_deteccao: float):
+        super().__init__(spawn, tamanho, velocidade)
         self.__spawn = spawn
         self.__raio_deslocamento = raio_deslocamento
         self.__raio_deteccao = raio_deteccao
-        self.__coordenada_jogador = spawn #eh para ser atualizado pelo codigo, talvez mudar isso depois
+        # eh para ser atualizado pelo codigo, talvez mudar isso depois
+        self.__coordenada_jogador = spawn
 
-    def decideDirecao(self): #praticamente um versor da direcao de deslocamento
+    def decideDirecao(self):  # praticamente um versor da direcao de deslocamento
         if(self.__coord.calculaDistancia(self.__coordenada_jogador) <= self.__raio_deteccao):
-            self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(self.__coord, self.__coordenada_jogador)
+            self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(
+                self.__coord, self.__coordenada_jogador)
         elif(self.__coord.calculaDistancia(self.__spawn) > self.__raio_deslocamento):
-            self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(self.__coord, self.__spawn)
+            self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(
+                self.__coord, self.__spawn)
         else:
-            self.__direcao_deslocamento = Coordenada(0,0) #talvez implementar alguma caminhada aleatoria depois
+            # talvez implementar alguma caminhada aleatoria depois
+            self.__direcao_deslocamento = Coordenada(0, 0)
 
-    def colidiu(self, coord:Coordenada):
-        pass #decicir o que fazer (voltar para spawn, ser jogado para tras, ...)
+    def colidiu(self, coord: Coordenada):
+        # decicir o que fazer (voltar para spawn, ser jogado para tras, ...)
+        pass
+
+    def desenhar(self, display):
+        cor = (255, 0, 0) #vermelho
+        return super().desenhar(display, cor)
--- prototipo/Jogador.py
+++ prototipo/Jogador.py
@@ -4,16 +4,17 @@ from Tamanho import Tamanho
 from Item import Item
 from PontoEntrega import PontoEntrega
 
+
 class Jogador(Movel):
-    def __init__(self, coord:Coordenada, tamanho:Tamanho, velocidade:float):
+    def __init__(self, coord: Coordenada, tamanho: Tamanho, velocidade: float):
         super().__init__(coord, tamanho, velocidade)
         self.__item_carregado = None
-        #remover pode_pegar_item e pode_entregar_item
+        # remover pode_pegar_item e pode_entregar_item
 
-    def pegarItem(self, item:Item) -> bool:
+    def pegarItem(self, item: Item) -> bool:
         if(self.__coord.calculaDistancia(item.coord) <= item.raio_interacao):
             self.__item_carregado = item
-            #talvez fazer alguma coisa com o item aqui
+            # talvez fazer alguma coisa com o item aqui
             return True
         else:
             return False
@@ -21,23 +22,32 @@ class Jogador(Movel):
     def removerItem(self):
         self.__item_carregado = None
 
-    def entregarItem(self, ponto_entrega:PontoEntrega) -> bool:
+    def entregarItem(self, ponto_entrega: PontoEntrega) -> bool:
         if(self.__coord.calculaDistancia(ponto_entrega.coord) <= ponto_entrega.raio_interacao):
             self.removerItem()
             return True
         else:
             return False
 
-    def colidiu(self, coord:Coordenada):
+    def colidiu(self, coord: Coordenada):
         self.removerItem()
-        #expandir depois
+        # expandir depois
 
-    def decideDirecao(self, cima:bool, baixo:bool, direita:bool, esquerda:bool):
+    def decideDirecao(self, cima: bool, baixo: bool, direita: bool, esquerda: bool):
         horizontal = 0
         vertical = 0
-        if(cima): vertical += 1
-        if(baixo): vertical -= 1
-        if(direita): horizontal += 1
-        if(esquerda): horizontal -= 1
+        if(cima):
+            vertical += 1
+        if(baixo):
+            vertical -= 1
+        if(direita):
+            horizontal += 1
+        if(esquerda):
+            horizontal -= 1
 
-        self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(Coordenada(0,0), Coordenada(horizontal, vertical))
+        self.__direcao_deslocamento = Coordenada.versorEntreCoordenadas(
+            Coordenada(0, 0), Coordenada(horizontal, vertical))
+
+    def desenhar(self, display):
+        cor = (0, 0, 255)  # azul
+        return super().desenhar(display, cor)
