5d03df8ecf25958af6f4c522d533e8be71c93fbb
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2022-02-09 15:09:22 -0300

    Create MenuPrincipal.py

---
+++ prototipo/MenuPrincipal.py
@@ -0,0 +1,19 @@
+
+  
+import pygame, sys
+from pygame.locals import *
+import Menu
+
+class MenuPrincipal(Menu):
+    def __init__(self, controladorJogo):
+        Menu.__init__(self, controladorJogo)
+        pass
+
+    def display_menu(self): 
+        pass
+ 
+    def move_cursor(self): 
+        pass
+
+    def checa_entrada(self):
+        pass
