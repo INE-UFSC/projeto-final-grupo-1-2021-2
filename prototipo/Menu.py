3e29a97418c99f98b1e017444b64285a08f6eee4
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2022-02-28 11:00:40 -0300

    Update Menu.py

--- prototipo/Menu.py
+++ prototipo/Menu.py
@@ -2,6 +2,7 @@ import pygame, sys
 from pygame.locals import *
 
 class Menu:
-    def __init__(self, controladorJogo):
-        self.controladorJogo = controladorJogo
+    def __init__(self):
+        pass
+    
             
