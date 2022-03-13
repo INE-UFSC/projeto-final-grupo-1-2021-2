5a810f9f0cbde7c14cb7af2f188fb68c3985ed76
Brenda-Machado <araucaria2011@gmail.com>
2022-03-13 12:39:53 -0300

    caminho das imagens

--- versao_final/GerenciadorImagens.py
+++ versao_final/GerenciadorImagens.py
@@ -11,8 +11,37 @@ class GerenciadorImagens(metaclass=Singleton):
             #vamos seguir uma estrutura que cada um é um dicionario?
             'jogador':{
                 'teste':'teste.PNG' #'parado1':None, 'parado2':None, etc
+            },
+            'supermercado':{
+                'mapa':'Supermercado/Mapa_supermercado.png' , 'balcao1': 'Supermercado/Balcao_G_vazio.png',
+                'balcao2': 'Supermercado/Balcao_G_verduras.png', 'balcao3': 'Supermercado/Balcao_P_frutas.png',
+                'balcao4': 'Supermercado/Balcao_P_verduras.png', 'freezer1': 'Supermercado/Freezer_C_fechado.png',
+                'freezer2': 'Supermercado/Freezer_C_pacotes.png', 'prateleira1': 'Supermercado/Prateleira_C_caixas.png',
+                'prateleira2': 'Supermercado/Prateleira_C_garrafas.png', 'prateleira3': 'Supermercado/Prateleira_G_vazia.png'
+            }, #É um dos inimigos_obstáculo, tem frames
+            'carrinho_compras' : {
+                'carrinho_direita': 'Supermercado/Carrinho_direita.png', 'carrinho_esquerda': 'Supermercado/Carrinho_esquerda.png',
+                 'carrinho_frente': 'Supermercado/Carrinho_frente_e_tras.png', 'carrinho_tras': 'Supermercado/Carrinho_frente_e_tras.png'
+
+            },
+            'cozinha':{
+                'mapa':'Cozinha/Mapa_cozinha.png' , 'balcao1': 'Cozinha/Balcao_comprido_vazio.png',
+                'balcao2': 'Cozinha/Balcao_G_cheio.png', 'balcao3': 'Cozinha/Balcao_m_vazio.png',
+                'balcao4': 'Cozinha/Balcao_p_utensilio.png', 'balcao5': 'CozinhaBalcao_p.png',
+                'geladeira': 'Cozinha/Geladeira.png', 'fogao': 'Cozinha/Fogao.png'
+            }, #Inimigo obstaculo
+            'carrinho_limpeza' : {
+                'carrinho_direita': 'Cozinha/Carrinho_limpeza_direita.png', 'carrinho_esquerda': 'Cozinha/Carrinho_limpeza_esquerda.png',
+                 'carrinho_frente': 'Cozinha/Carrinho_limpeza_frente.png', 'carrinho_tras': 'Cozinha/Carrinho_limpeza_tras.png'
+            
+            },
+            'item' : {
+                'acucar': 'Comida/Acucar.png', 'alho': 'Comida/Alho.png', 'carne': 'Comida/Carne.png', 
+                'leite': 'Comida/Leite.png','manteiga': 'Comida/Manteiga.png', 'ovo': 'Comida/Ovo.png', 
+                'pacote': 'Comida/Pacote.png', 'queijo': 'Comida/Queijo.png','tomate': 'Comida/Tomate.png', 
+                'churrasco': 'Comida/Churrasco.png', 'macarronada': 'Comida/Macarronada.png', 'salada': 'Comida/Salada.png',
+                 'sopa': 'Comida/Sopa.png', 'sorvete': 'Comida/Sorvete.png'
             }
-            #'inimigos_pessoa':{}, etc
         }
         self.__imagens_carregadas = None
         self.__carregarImagens()
