794aafe94721293b4653db9ecc1929f936ff8d68
gabi-fs <gabrielafdasilveira@gmail.com>
2022-03-17 11:44:09 -0300

    classe abstrata menu e herança menus

--- versao_final/Menu.py
+++ versao_final/Menu.py
@@ -1,8 +1,55 @@
+from typing import Tuple
 import pygame, sys
 from pygame.locals import *
+from GerenciadorImagens import GerenciadorImagens
+from abc import abstractmethod, ABC
 
-class Menu:
-    def __init__(self):
-        pass
+class Menu(ABC):
+    def __init__(self, tamanho: Tuple):
+        self.__tamanho_display = self.largura, self.altura = tamanho[0], tamanho[1]
+        self.__display = pygame.display.set_mode(
+            self.__tamanho_display, pygame.HWSURFACE)
+        self.__branco = ((255, 255, 255))
+        self.__fonte = 'PressStart2P-vaV7.ttf'
+        self.__fundo = GerenciadorImagens().getSprite(
+            'fundo_menu', 'fundo_menu', self.largura, self.altura)
+
+
+    @property
+    def tamanho_display(self):
+        return self.__tamanho_display
+        
+    @property
+    def display(self):
+        return self.__display
+
+    @property
+    def branco(self):
+        return self.__branco
+
+    @property
+    def fonte(self):
+        return self.__fonte
+
+    @property
+    def fundo(self):
+        return self.__fundo
+
+    @fonte.setter
+    def fonte(self, fonte):
+        self.__fonte = fonte
+
+    @fundo.setter
+    def fundo(self, fundo):
+        self.__fundo = fundo
+        
+    def desenha_texto(self, texto, tamanho, x, y, cor, fonte):
+        font = pygame.font.Font(fonte, tamanho)
+        text_surface = font.render(texto, True, cor)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        self.__display.blit(text_surface, text_rect) 
     
-            
+    @abstractmethod
+    def display_menu(self):
+        pass
--- versao_final/MenuTutorial.py
+++ versao_final/MenuTutorial.py
@@ -4,42 +4,43 @@ from Menu import Menu
 from GerenciadorImagens import GerenciadorImagens
 
 class MenuTutorial(Menu):
-    def __init__(self):
-        self.__fonte = 'PressStart2P-vaV7.ttf'
-        self.__branco = ((255, 255, 255))
-        self.tamanho_display = self.largura, self.altura = 720*2, 480*2
-        self.__display = pygame.display.set_mode(
-            self.tamanho_display, pygame.HWSURFACE)
-        self.__fundo = GerenciadorImagens().getSprite(
-            'fundo_menu', 'fundo_menu', self.largura, self.altura)
+    def __init__(self, tamanho):
+        #self.fonte = 'PressStart2P-vaV7.ttf'
+        #self.branco = ((255, 255, 255))
+        #self.tamanho_display = self.largura, self.altura = 720*2, 480*2
+        #self.__display = pygame.display.set_mode(
+            #self.tamanho_display, pygame.HWSURFACE)
+        #self.__fundo = GerenciadorImagens().getSprite(
+            #'fundo_menu', 'fundo_menu', self.largura, self.altura)
+        super().__init__(tamanho)
     
-    def desenha_texto(self, texto, tamanho, x, y, cor, fonte):
-        font = pygame.font.Font(fonte, tamanho)
-        text_surface = font.render(texto, True, cor)
-        text_rect = text_surface.get_rect()
-        text_rect.center = (x, y)
-        self.__display.blit(text_surface, text_rect)
+    #def desenha_texto(self, texto, tamanho, x, y, cor, fonte):
+        #font = pygame.font.Font(fonte, tamanho)
+        #text_surface = font.render(texto, True, cor)
+        #text_rect = text_surface.get_rect()
+        #text_rect.center = (x, y)
+        #self.__display.blit(text_surface, text_rect)
            
     def display_menu(self):
-        self.__display.fill((0, 0, 0))
-        self.__display.blit(self.__fundo, (0,0))
+        self.display.fill((0, 0, 0))
+        self.display.blit(self.fundo, (0,0))
         self.desenha_texto('Tutorial', 60, self.largura / 2,
-                           self.altura / 8, self.__branco, self.__fonte)
+                           self.altura / 8, self.branco, self.fonte)
         self.desenha_texto("Andar para Direira:  D", 25, self.largura/2,
-                           self.altura/2 - 190, self.__branco, self.__fonte)
+                           self.altura/2 - 190, self.branco, self.fonte)
         self.desenha_texto("Andar para Esquerda: A", 25, self.largura/2,
-                           self.altura/2 - 140, self.__branco, self.__fonte)
+                           self.altura/2 - 140, self.branco, self.fonte)
         self.desenha_texto("Andar para Cima: W", 25, self.largura/2,
-                           self.altura/2 - 90, self.__branco, self.__fonte)
+                           self.altura/2 - 90, self.branco, self.fonte)
         self.desenha_texto("Andar para Baixo: S", 25, self.largura/2,
-                           self.altura/2 - 40, self.__branco, self.__fonte)
+                           self.altura/2 - 40, self.branco, self.fonte)
         self.desenha_texto("Pegar Item: Espaço", 25, self.largura/2,
-                           self.altura/2 + 10, self.__branco, self.__fonte)
+                           self.altura/2 + 10, self.branco, self.fonte)
         self.desenha_texto("Entregar Item: Espaço", 25, self.largura/2,
-                           self.altura/2 + 60, self.__branco, self.__fonte)
+                           self.altura/2 + 60, self.branco, self.fonte)
         self.desenha_texto("Pausa: Esc", 25, self.largura/2,
-                           self.altura/2 + 110, self.__branco, self.__fonte)                   
+                           self.altura/2 + 110, self.branco, self.fonte)                   
         self.desenha_texto("Voltar: Backspace", 20, self.largura/2 - 200,
-                           self.altura/2 + 190, self.__branco, self.__fonte)
+                           self.altura/2 + 190, self.branco, self.fonte)
         self.desenha_texto("Avançar: Enter", 20, self.largura/2 + 200,
-                           self.altura/2 + 190, self.__branco, self.__fonte)
+                           self.altura/2 + 190, self.branco, self.fonte)
